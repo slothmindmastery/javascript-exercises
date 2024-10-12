15e624c792c5eabcca6a08827f27f83a80fd144f
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-12 14:53:30 +0100

    Create function to convert temperatures to different units

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  // to one decimal place
+  // (x - 32) * (5 / 9)
+  let celcius = (farenheit - 32) * (5 / 9);
+  let rounded = Math.round(celcius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  // (x * (9 / 5) + 32)
+  let farenheit = (celcius * (9 / 5) + 32);
+  let rounded = Math.round(farenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
