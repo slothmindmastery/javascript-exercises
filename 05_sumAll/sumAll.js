d9c419b50ba7fdd143b4c24f13bf00acbc97e7c8
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-12 14:21:30 +0100

    Create function to return sum of all values between and including two arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+    return "ERROR";
+  } 
+  let total = 0;
+  let startNumber = 0;
+  let endNumber = 0;
+  if (x < y) {
+    startNumber = x;
+    endNumber = y;
+  }
+  else {
+    startNumber = y;
+    endNumber = x;
+  }
+  for (let i = startNumber; i <= endNumber; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
