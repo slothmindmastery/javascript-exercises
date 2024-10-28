58b1e1b19ad8f01645656e6abbdf2019da44eef4
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-28 23:42:35 +0000

    Create function to find oldest person in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let birth = 0;
+  let death = 0;
+  let oldestAge = 0;
+  let oldestPerson = null;
+
+  for (person of people) {
+    let age = 0;
+    birth = person.yearOfBirth;
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    death = person.yearOfDeath;;
+    age = death - birth;
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  }
+
+return oldestPerson;
 
 };
 
