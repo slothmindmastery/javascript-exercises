327f680e67eed28fbe130fd86b07fedaf9534e67
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-28 22:06:17 +0000

    Create function to check if string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (a) {
+  let arr = [...a];
+  const filtered = arr.filter(checkLetter);
 
+  function checkLetter(letter) {
+    if ((/[a-zA-Z0-9]/).test(letter)) {
+      return true;
+    }
+  }
+
+  for (let i = filtered.length - 1; i >= 0; i--) {
+    if (filtered[i].toLowerCase() != filtered[filtered.length - 1 - i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
