063c443c25fc3e1427feb44f8dfbfce865d8a07c
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-28 23:06:06 +0000

    Create function to calculate number in Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
-};
+  fibonacciSequence = [0, 1];
+
+  if (typeof num === "string") {
+    num = parseInt(num);
+  }
+  if (num < 0) {
+    return "OOPS"; 
+  }
+  else if (num == 0) {
+    return 0;
+  }
+  else if (num == 1) {
+    return 1;
+  }
+  else {
+    for (i = 2; i <= num; i++) {
+      fibonacciSequence[i] = (fibonacciSequence[i-1] + fibonacciSequence[i-2]);
+    }
+  }
+
+return fibonacciSequence[num];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
