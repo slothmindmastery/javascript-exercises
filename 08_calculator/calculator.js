e02a9569eb4e4f89714fb80b73f57bed7013f8d5
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-28 19:17:16 +0000

    Complete calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  const answer = a.reduce((total, number) => {
+    return total + number;
+  }, 0);
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const answer = a.reduce((total, number) => {
+    return total * number;
+  });
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) {
+    return 1;
+  }
+  else {
+    let total = a;
+    for (i = a - 1; i > 0; i--) {
+      total = total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
